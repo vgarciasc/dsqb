ae65bdac00581bc93d4602e88897700f4a358299
Vinícius Garcia <vingarciasc@gmail.com>
2017-07-24 23:38:59 -0300

    v0.1.3
    added:
    - bow: player can charge and shoot arrows.

--- Assets/Scripts/Battle/Player.cs
+++ Assets/Scripts/Battle/Player.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Weapon { BOW };
+
 public class Player : MonoBehaviour {
 
+	//references
 	Rigidbody2D rb;
-	
-	[Header("Character Attributes")]
-	[HideInInspector]
-	public float health; //from 0 to 100
-	[HideInInspector]
-	public float stamina; //from 0 to 100
+
+	float charge_time = 3f; //time in seconds to complete a charge cycle
+	bool is_charging_shot = false;
+
+	//properties
+	public Weapon current_weapon = Weapon.BOW;
+	public float charge = 0f;
+	public float health = 1f;
+	public float stamina = 1f;
 
 	[Header("Physics Properties")]
 	[SerializeField]
 	float speed;
+
+	[Header("Prefab References")]
+	[SerializeField]
+	GameObject arrowPrefab;
 
 	#region Start
 		void Start() {
@@ -28,15 +38,125 @@ public class Player : MonoBehaviour {
 
 	#region Update and Handlers
 		void Update () {
-			Handle_Input();
+			Handle_Movement();
+			Handle_Stamina();
+			Handle_Weapon();
 		}
 
-		void Handle_Input() {
+		void Handle_Movement() {
 			float horizontal = Input.GetAxis("Horizontal");
 			float vertical = Input.GetAxis("Vertical");
 
+			float speed = this.speed;
+
+			if (is_charging_shot) {
+				speed /= 4f; //if player is charging shot, he gets slower
+			}
+
 			rb.velocity = new Vector2(horizontal, vertical) * speed;
+		}
+
+		void Handle_Weapon() {
+			switch (current_weapon) {
+				case Weapon.BOW:
+					Handle_Weapon_Bow();
+					break;
+			}
 		}
 	#endregion
 
+	#region Charge
+		void Add_Charge() {
+			charge += Time.deltaTime / charge_time;
+			charge = Mathf.Clamp(charge, 0f, 1f);
+		}
+
+		void Reset_Charge() {
+			charge = 0f;
+		}
+	#endregion
+
+	#region Weapons
+		void Handle_Weapon_Bow() {
+			if (Input.GetButton("Fire1")) {
+				Start_Bow();
+			}
+
+			if (Input.GetButtonUp("Fire1")) {
+				Release_Bow(charge);
+			}
+		}
+
+		void Start_Bow() {
+			is_charging_shot = true;
+			Stop_Stamina_Recovery();
+			Add_Charge();
+		}
+
+		void Release_Bow(float charge) {
+			is_charging_shot = false;
+			Reset_Charge();
+			Start_Stamina_Recovery();
+
+			float minimumChargeForBow = 0.33f;
+			if (charge < minimumChargeForBow) {
+				return;	
+			}
+			
+			GameObject arrow = Instantiate(
+				arrowPrefab,
+				this.transform.position,
+				Quaternion.identity
+			);
+
+			float arrowSpeed = 50 * charge;
+			arrow.GetComponentInChildren<Rigidbody2D>().AddForce(
+				Vector2.up * arrowSpeed,
+				ForceMode2D.Impulse
+			);
+		}
+	#endregion
+
+	#region
+
+		//stamina variables
+		float bow_stamina_depletion = 2f;
+		Coroutine stamina_recovery = null;
+		float stamina_recovery_time = 4f;
+
+		void Handle_Stamina() {
+			if (is_charging_shot) {
+				stamina -= Time.deltaTime / bow_stamina_depletion; 
+			}
+
+			stamina = Mathf.Clamp(stamina, 0f, 1f);
+
+			if (stamina == 0f && is_charging_shot) {
+				Release_Bow(charge);
+			}
+		}
+
+		void Start_Stamina_Recovery() {
+			Stop_Stamina_Recovery();
+			stamina_recovery = StartCoroutine(Recover_Stamina());
+		}
+
+		void Stop_Stamina_Recovery() {
+			if (stamina_recovery != null) {
+				StopCoroutine(stamina_recovery);
+				stamina_recovery = null;
+			}
+		}
+
+		IEnumerator Recover_Stamina() {
+			yield return new WaitForSeconds(0.5f);
+
+			while (stamina < 1f) {
+				stamina += Time.deltaTime / stamina_recovery_time;
+				yield return new WaitForEndOfFrame();
+			}
+
+			stamina_recovery = null;
+		}
+	#endregion
 }
--- Assets/Scripts/Battle/PlayerUIManager.cs
+++ Assets/Scripts/Battle/PlayerUIManager.cs
@@ -15,18 +15,47 @@ public class PlayerUIManager : MonoBehaviour {
 	Image health_indicator;
 	[SerializeField]
 	Image stamina_indicator;
+	[SerializeField]
+	Image weapon_indicator;
+
+	[Header("Sprites")]
+	[SerializeField]
+	List<Sprite> bow_sprites = new List<Sprite>();
 
 	void Update() {
 		Update_Health();
 		Update_Stamina();
+		Update_Weapon();
 	}
 
 	void Update_Health() {
-		health_indicator.fillAmount = player.health / 100;
+		health_indicator.fillAmount = player.health;
 	}
 
 	void Update_Stamina() {
-		stamina_indicator.fillAmount = player.stamina / 100;
+		stamina_indicator.fillAmount = player.stamina;
 	}
 
+	void Update_Weapon() {
+		switch (player.current_weapon) {
+			case Weapon.BOW:
+				Update_Bow();
+				break;
+		}
+	}
+
+	void Update_Bow() {
+		float unit = 1f / bow_sprites.Count;
+
+		for (int i = 1; i < bow_sprites.Count; i++) {
+			if (player.charge > unit * (i-1) &&
+				player.charge < unit * (i)) {
+				weapon_indicator.sprite = bow_sprites[i];
+			}
+		}
+
+		if (player.charge <= 0.01f) {
+			weapon_indicator.sprite = bow_sprites[0];
+		}
+	}
 }
